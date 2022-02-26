9220705df083bd3372d73c8755c29a62c3840920
d1snin <~@d1s.uno>
2022-02-26 15:31:46 +0300

    made `DeferReplier` functional

--- src/main/kotlin/uno/d1s/slash/listener/replier/DeferReplier.kt
+++ src/main/kotlin/uno/d1s/slash/listener/replier/DeferReplier.kt
@@ -2,7 +2,7 @@ package uno.d1s.slash.listener.replier
 
 import net.dv8tion.jda.api.requests.restaction.interactions.ReplyCallbackAction
 
-public interface DeferReplier {
+public fun interface DeferReplier {
 
     public fun reply(action: ReplyCallbackAction): ReplyCallbackAction
 }
